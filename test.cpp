fe1418ccbf0ed4d11d90df10f0163e989ac1d5b1
SKR301 <chunnu.bihat@gmail.com>
2022-03-26 13:54:50 +0530

    #1 added unit tests

--- test.cpp
+++ test.cpp
@@ -11,7 +11,8 @@ int main(int argc, char const *argv[])
 	std::string testEncodedString1 = "00000101000100000110001011010110001010";
 	std::string testEncodedString2 = "1101";
 
-
+	int passCount = 0;
+	int total = 5;
 
 
 	AdaptiveHuffman AH;
@@ -22,6 +23,7 @@ int main(int argc, char const *argv[])
 	std::string encodedTestString1 = AH.encode(testDecodedString1);
 	if(encodedTestString1 == testEncodedString1){
 		std::cout<<"\n\nPASSED!\n";
+		passCount++;
 	} else {
 		std::cout<<"\n\nFAILED!\n";
 	}
@@ -34,6 +36,7 @@ int main(int argc, char const *argv[])
 	std::string encodedTestString2 = AH.encode(testDecodedString2);
 	if(encodedTestString2 == testEncodedString2){
 		std::cout<<"\n\nPASSED!\n";
+		passCount++;
 	} else {
 		std::cout<<"\n\nFAILED!\n";
 	}
@@ -41,6 +44,48 @@ int main(int argc, char const *argv[])
 	std::cout<<"\nExpected output\t"<< testEncodedString2;
 
 
+	std::cout<<"\n";
+	std::cout<<"\n\n----------Decode Test1:------------";
+	std::cout<<"\nInput String:\t"<<testEncodedString1;
+	std::string decodedTestString1 = AH.decode(testEncodedString1);
+	if(decodedTestString1 == testDecodedString1){
+		std::cout<<"\n\nPASSED!\n";
+		passCount++;
+	} else {
+		std::cout<<"\n\nFAILED!\n";
+	}
+	std::cout<<"\nActual output:\t"<< decodedTestString1;
+	std::cout<<"\nExpected output\t"<< testDecodedString1;
+
+	std::cout<<"\n";
+	std::cout<<"\n\n----------Decode Test2:------------";
+	std::cout<<"\nInput String:\t"<<testEncodedString2;
+	std::string decodedTestString2 = AH.decode(testEncodedString2);
+	if(decodedTestString1 == testDecodedString2){
+		std::cout<<"\n\nPASSED!\n";
+		passCount++;
+	} else {
+		std::cout<<"\n\nFAILED!\n";
+	}
+	std::cout<<"\nActual output:\t"<< decodedTestString2;
+	std::cout<<"\nExpected output\t"<< testDecodedString2;
+
+
+	std::cout<<"\n";
+	std::cout<<"\n\n----------Encode-Decode Test1:------------";
+	std::cout<<"\nInput String:\t"<<testDecodedString3;
+	std::string retrievedString = AH.decode(AH.encode(testDecodedString3));
+	if(retrievedString == testDecodedString3){
+		std::cout<<"\n\nPASSED!\n";
+		passCount++;
+	} else {
+		std::cout<<"\n\nFAILED!\n";
+	}
+	std::cout<<"\nActual output:\t"<< retrievedString;
+	std::cout<<"\nExpected output\t"<< testDecodedString3;
+
+
+	std::cout<<"\n\n\n\tResult: "<<passCount<<" / "<<total;
 
 	std::cout<<"\n\n";
 	system("pause");
